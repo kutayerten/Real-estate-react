6ad58ce5188a22dcdee4fe40b5f2c977871c700f
kutay <kutayerten@gmail.com>
2024-06-27 20:44:43 +0300

    Map added to the List page

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,5 +1,6 @@
 import Card from '../../components/card/card';
 import Filter from '../../components/filter/filter';
+import Map from '../../components/map/map';
 import { listData } from '../../lib/dummydata'
 import './listPage.scss'
 
@@ -16,7 +17,9 @@ export default function ListPage() {
             ))}
         </div>
       </div>
-      <div className='mapContainer'>map</div>
+      <div className='mapContainer'>
+        <Map items={data}/>
+      </div>
     </div>
   )
 }
